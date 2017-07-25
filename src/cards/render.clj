9873fff0cb56df8030a98616b7b2d90227c1a4b4
Sean Vincent <sean@searchspring.com>
2017-07-24 21:52:24 -0600

    Adds ansi-str rendering of card(s)
    * Adds dependancy on terminal.ansi/ansi-str
    * card->ansi-str - render an ansi string representing a card,
                       using the same options as terminal.ansi/ansi-str.
    * cards->str - render a seq of cards using card->str, and an optional
                   separator.
    * cards->ansi-str - like cards->str, but with ansi strings.

--- src/cards/render.clj
+++ src/cards/render.clj
@@ -1,4 +1,6 @@
-(ns cards.core)
+(ns cards.core
+  (:require [clojure.string :as str]
+            [terminal.ansi :refer [ansi-str]]))
 
 (def suit->char
   {:clubs \u2663
@@ -25,4 +27,38 @@
   "Returns the string representation of a card."
   [{:keys [rank suit]}]
   (str (rank->str rank) \space (suit->char suit)))
+
+(defn card->ansi-str
+  "Returns an ansi string representation of a card.
+  Accepts the same options as terminal.ansi/ansi-str.
+  Default ansi options are to use the predefined color
+  for the card's suit."
+  ([card]
+   (card->ansi-str card (suit->color (:suit card))))
+  ([card & ansi-opts]
+   (let [card-str (card->str card)]
+     (apply ansi-str card-str ansi-opts))))
+
+(defn cards->str
+  "Returns the string representation of the given cards.
+  Renders each card using card->str, then joins then with
+  the given separator (defaults to \newline)."
+  ([cards]
+   (cards->str \newline))
+  ([cards sep]
+   (str/join sep (map card->str cards))))
+
+(defn cards->ansi-str
+  "Returns the ansi string representation of the given cards.
+  Accepts the same options as terminal.ansi/ansi-str.
+  If specified, ansi options apply to all cards.
+  If unspecified, acts as if calling card->ansi-str on each
+  card without specified ansi options (i.e. each card will be
+  the color of that card's suit)."
+  ([cards]
+   (cards->ansi-str cards \newline))
+  ([cards sep]
+   (str/join sep (map card->ansi-str cards)))
+  ([cards sep & ansi-opts]
+   (str/join sep (map #(apply card->ansi-str % ansi-opts) cards))))
 
